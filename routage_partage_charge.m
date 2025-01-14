7b35b0335d22e52b798af34c72812a926be746e0
lsy4701 <165167579+lsy4701@users.noreply.github.com>
2025-01-14 11:39:59 +0100

    Update routage_partage_charge.m

--- routage_partage_charge.m
+++ routage_partage_charge.m
@@ -1,9 +1,10 @@
-function [chemins] = routage_partage_charge(appel)
+function [chemins] = routage_partage_charge(mat_etats_liens,appel, nb)
 %ROUTAGE_PARTAGE_CHARGE Réalise le routage par partage de charge d'un
 % Réseau téléphonique
 chemins=zeros(5);
 mat_probas=[0 0.06 0 0.47 0.47 ; 0.05 0 0.05 0.45 0.45 ; ...
     0 0.06 0 0.47 0.47 ; 0.07 0.07 0.07 0 0.79 ; 0.07 0.07 0.07 0.79 0];
+mat_probas = mat_probas*nb;
 mat_probas=1-mat_probas;
 source=appel(1);
 dest=appel(2);
