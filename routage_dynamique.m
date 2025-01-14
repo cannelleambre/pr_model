8bb63ea3cc124d818f9d929fe2f89346452fc595
lsy4701 <165167579+lsy4701@users.noreply.github.com>
2025-01-14 11:11:58 +0100

    Update routage_dynamique.m

--- routage_dynamique.m
+++ routage_dynamique.m
@@ -1,20 +1,19 @@
 function [chemins] = routage_dynamique(mat_etats_liens,appel)
 %UNTITLED Réalise le routage dynamique d'un réseau téléphonique
 chemins=zeros(5);
-mat_etats_liens=1000-mat_etats_liens;
+mat_etats_liens2=1000-mat_etats_liens;
 source=appel(1);
 dest=appel(2);
 
 % Algorithme de Dijkstra
-[~,L] = dijkstra(mat_etats_liens,source,dest);
+[~,L] = dijkstra(mat_etats_liens2,source,dest);
 
 % Création de la matrice des chemins
 for i=1:length(L)-1
-    if (L(i)==0)
-        chemins(L(i)=0);
+    if (mat_etats_liens(L(i),L(i+1))>0)
+        chemins(L(i),L(i+1))=1; 
     end
-    chemins(L(i),L(i+1))=1;
+    
 end
 
 end
-
